35e91b5edbb19e93fe4580c768f98ffa5d307fce
MildredJessica <nwonyejessica@gmail.com>
2023-10-02 05:39:26 +0100

    Change the file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,21 +2,20 @@
 """Function for the Pascal Triangle"""
 
 
-def pascalTriangle(n):
+def pascal_triangle(n):
     """Pascal Triangle"""
     new_list = []
-    for l in range(0, n):
+    if n <= 0:
+        return new_list
+    for l in range( n):
         first_list = []
-        for i in range(0, l + 1):
-            first_list.append(bCoeffient(l, i))
+        for i in range(l + 1):
+            first_list.append(factorial(l)//(factorial(i)*factorial(l-i)))
+            print(first_list)
         new_list.append(first_list)
+    return new_list
 
 
-def bCoeffient(n , k):
-    r = 1
-    if k > n - k:
-        k = n - k
-    for i in range(0, k):
-        r = r * (n - i)
-        r = r // (i - 1)
-    return r
+def factorial(n):
+    """Calculates the factorial"""
+    return 1 if (n == 1 or n == 0) else n * factorial(n - 1)
